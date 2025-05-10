de405b9400ac0944908dc8326f3ca19c70def93a
Your Name <taiirbekovag@gmail.com>
2025-05-10 17:47:13 +0600

    Numbers in Python

---
+++ Python_numbers.py
@@ -0,0 +1,17 @@
+#int,float,complex
+a =2
+b= 4.3
+c =6j
+print(type(a))
+print(type(b))
+print(type(c))
+
+
+print("====== Changed types of numbers =========")
+
+x = float(a)
+y = int(b)
+z = complex(c)
+print(type(x))
+print(type(y))
+print(type(z))
